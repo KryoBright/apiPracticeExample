f655c67254af2138922e6ae4b15bd601b020feab
delet-dis <efimov.036@gmail.com>
2024-04-13 09:13:06 +0700

    🗃️ basic project structure added

---
+++ src/main/java/com/hits/backend2api/Backend2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.hits.backend2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend2ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hits/backend2api/Backend2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hits.backend2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
